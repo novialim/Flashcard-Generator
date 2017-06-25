76fb0e27c97285831f5c9760af0235bd9e5dc5c1
novialim <novialim@gmail.com>
2017-06-24 22:41:25 -0700

    backend frontend completed

---
+++ Cloze.js
@@ -0,0 +1,30 @@
+/**
+ * Cloze Flashcard
+ */
+
+"use strict";
+
+function ClozeCard (fulltext, cloze){
+
+	this.fulltext = fulltext;
+	this.cloze = cloze;
+	this.partial = function(){
+		if(this.fulltext.toLowerCase().includes(this.cloze.toLowerCase())){
+			this.clozeErr = false;
+			return this.fulltext.toLowerCase().replace(this.cloze.toLowerCase(),"...");
+		}
+		else{
+			console.log("Error: "+this.cloze+" does not exists.");
+			this.clozeErr = true;
+		}
+	}
+}
+
+ClozeCard.prototype.printCard = function(){
+	console.log("-------------------------------");
+	console.log("The full text is: " + this.fulltext);	
+	console.log("-------------------------------");
+
+}
+
+module.exports = ClozeCard;
